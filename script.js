1e174281960ddfad95034c819de563f482a07059
Matheus Barros <matheusbarros1911@gmail.com>
2023-05-10 16:20:06 -0300

    Criado botão que muda de cor

---
+++ script.js
@@ -0,0 +1,32 @@
+function clicar() {
+
+    const paragrafo = document.getElementById('botao')
+
+        paragrafo.innerText = 'Clicou!'
+        paragrafo.style.color = 'Black'
+        paragrafo.style.border = '100px solid Yellow'
+        paragrafo.style.background = 'Yellow'
+
+}
+
+function entrou() {
+
+    const paragrafo = document.getElementById('botao')
+
+        paragrafo.innerText = 'Entrou!'
+        paragrafo.style.color = 'Black'
+        paragrafo.style.border = "100px solid Green"
+        paragrafo.style.background = "Green"
+    
+}
+
+function saiu() {
+
+    const paragrafo = document.getElementById('botao')
+
+        paragrafo.innerText = 'Saiu!'
+        paragrafo.style.color = 'Black'
+        paragrafo.style.border = '100px solid Red'
+        paragrafo.style.background = 'Red'
+
+}
